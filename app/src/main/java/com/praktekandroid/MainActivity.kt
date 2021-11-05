23583a9628663779556d9c6cd6d781872a1bd74c
Fthullh <fthullh99@gmail.com>
2021-11-05 20:00:01 +0800

    Menghubungkan Kotlin dengan komponen

--- app/src/main/java/com/praktekandroid/MainActivity.kt
+++ app/src/main/java/com/praktekandroid/MainActivity.kt
@@ -10,5 +10,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val simpanButton = findViewById<Button>(R.id.simpanButton)
+        val batalButton = findViewById<Button>(R.id.batalButton)
+
+        val namaEditText = findViewById<EditText>(R.id.namaEditText)
+        val emailEditText = findViewById<EditText>(R.id.emailEditText)
+
+        simpanButton.setOnClickListener {
+            namaEditText.setText("")
+            emailEditText.setText("")
+        }
+
+        batalButton.setOnClickListener {
+            finish()
         }
     }
+}
