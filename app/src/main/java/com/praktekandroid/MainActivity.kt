5165c295725288940c5d2d085a7084bf74ba1889
Fthullh <fthullh99@gmail.com>
2021-11-26 22:09:52 +0800

    Hasil Activity

--- app/src/main/java/com/praktekandroid/MainActivity.kt
+++ app/src/main/java/com/praktekandroid/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.praktekandroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -16,9 +17,26 @@ class MainActivity : AppCompatActivity() {
 
         val namaEditText = findViewById<EditText>(R.id.namaEditText)
         val emailEditText = findViewById<EditText>(R.id.emailEditText)
+        val handphoneEditText = findViewById<EditText>(R.id.handphoneEditText)
+        val tanggalLahirEditText = findViewById<EditText>(R.id.tanggalLahirEditText)
+        val jumlahMataKuliahEditText = findViewById<EditText>(R.id.jumlahMataKuliahEditText)
 
         simpanButton.setOnClickListener {
             Toast.makeText(this,"Hai Apa Kabar !!! ",Toast.LENGTH_SHORT).show()
+
+            val nama = namaEditText.text.toString()
+            val email = emailEditText.text.toString()
+            val handphone = handphoneEditText.text.toString()
+            val tanggalLahir = tanggalLahirEditText.text.toString()
+            val jumlahMataKuliah = jumlahMataKuliahEditText.text.toString().toInt()
+
+            val intent = Intent(this,HasilActivity::class.java)
+            intent.putExtra("nama", nama)
+            intent.putExtra("email", email)
+            intent.putExtra("handphone", handphone)
+            intent.putExtra("tanggalLahir", tanggalLahir)
+            intent.putExtra("jumlahMataKuiah", jumlahMataKuliah)
+            startActivity(intent)
         }
 
         batalButton.setOnClickListener {
